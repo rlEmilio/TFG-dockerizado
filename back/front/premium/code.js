8ab811ec8a6fca3c2ac9c0338187a0cacbc49dd0
Emilio <ruizlopezemilio2014@gmail.com>
2025-08-28 17:36:17 +0200

    update

--- back/front/premium/code.js
+++
@@ -1,8 +0,0 @@
-const btnPremium = document.getElementById("btn-premium");
-
-btnPremium.addEventListener("click", function() {
-    if (JSON.parse(sessionStorage.getItem("id")) != null) {
-        window.location.href = "../pago_premium/index.html";
-    }else
-        window.location.href = "../usuarios/iniciarsesion/index.html";
-})
